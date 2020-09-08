7ef2950f1c6d6c82d8c754b6ff3b6f3cd510e69e
geronda <70946840+geronda@users.noreply.github.com>
2020-09-08 14:34:19 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,4 @@
+$(".header-burger").click(function () {
+  $(".header-burger-navigation").toggleClass("header-burger-navigation-active");
+  $(".header-burger").toggleClass("header-burger-active");
+});
